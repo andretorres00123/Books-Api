1ace0606ee9f041139b55a050f424398917bf5c9
Andre Torres <atorres@devsu.com>
2019-07-21 23:20:54 -0500

    done testing

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI');
+
+if (process.env.ENV === 'Test') {
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+} else {
+  const db = mongoose.connect('mongodb://localhost/bookAPI-prod');
+}
+
+
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
@@ -17,6 +24,8 @@ app.get('/', (req, res) => {
   res.send('Welcome from Nodemon!');
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+module.exports = app;
